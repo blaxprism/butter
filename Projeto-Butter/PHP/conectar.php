db2a9f60c679c2bb70cd5db730144a758b3939c1
Fabrício José <90413455+blaxprism@users.noreply.github.com>
2022-07-20 00:57:11 -0300

    Delete Projeto-Butter directory

--- Projeto-Butter/PHP/conectar.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servidor = "localhost";
-$usuario = "root";
-$senhabanco = "";
-$banco="lorem";
-
-$conexao=mysqli_connect($servidor,$usuario,$senhabanco,$banco) or die("Erro na conexão");
-
-?> 
